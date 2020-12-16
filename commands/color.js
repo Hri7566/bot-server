41bf4b860b27792cacdbaad4190be05ac6d5cba0
Hri7566 <hri7566@gmail.com>
2020-12-16 08:40:10 -0500

    fix color

--- commands/color.js
+++ commands/color.js
@@ -1,9 +1,9 @@
 const Command = require('../bot/Command');
 const colorString = require('color-string');
 
-module.exports = new Command(["color","c"], `Usage: PREFIXcolor <color>`, 1, (msg, bot) => {
+module.exports = new Command(["color","c"], `Usage: PREFIXcolor <color>`, 0, (msg, bot) => {
     if (!msg.args[1]) {
-        return `${msg.p.name}, your color is ${bot.Color.getNearestColor(msg.p.color)}.`
+        return `${msg.p.name}, your color is ${bot.Color.getNearestColor(msg.p.color)} [${msg.p.color}].`
     } else {
         try {
             return `${msg.argcat} is ${bot.Color.getNearestColor(colorString.to.hex(colorString.get(msg.argcat).value))} [${colorString.to.hex(colorString.get(msg.argcat).value)}]`;
