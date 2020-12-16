61fbe16a21682d51b310a93150a48682e3f4ae33
Hri7566 <hri7566@gmail.com>
2020-12-16 08:34:28 -0500

    add about and color commands, update packages

--- commands/about.js
+++ commands/about.js
@@ -1,6 +1,6 @@
 const Command = require('../bot/Command');
 const pkg = require('../package.json');
 
-module.exports = new Command(["about","a"], `Usage: PREFIXabout`, 1, (msg, bot) => {
+module.exports = new Command(["about","a"], `Usage: PREFIXabout`, 0, (msg, bot) => {
     return pkg.description;
 }, 0, false);
---
+++ commands/color.js
@@ -0,0 +1,15 @@
+const Command = require('../bot/Command');
+const colorString = require('color-string');
+
+module.exports = new Command(["color","c"], `Usage: PREFIXcolor <color>`, 1, (msg, bot) => {
+    if (!msg.args[1]) {
+        return `${msg.p.name}, your color is ${bot.Color.getNearestColor(msg.p.color)}.`
+    } else {
+        try {
+            return `${msg.argcat} is ${bot.Color.getNearestColor(colorString.to.hex(colorString.get(msg.argcat).value))} [${colorString.to.hex(colorString.get(msg.argcat).value)}]`;
+        } catch (err) {
+            console.log(err);
+            return `${msg.argcat} is not a valid color.`;
+        }
+    }
+}, 0, false);
